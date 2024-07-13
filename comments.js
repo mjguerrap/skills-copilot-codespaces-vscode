1d80108ec0674ff6c7da3ef21605ffbec668d89c
Marcelo José Guerra Pereira <mjguerrap@gmail.com>
2024-07-13 18:28:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,9 @@
 // create web server
-var express = require('express');
-var app = express();
-
-// create a route
-app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
-});
-
-// start server
-app.listen(3000, function() {
-  console.log('Server is listening on port 3000');
-});
+// 1. create web server
+// 2. create a route for comments
+// 3. create a route for comments/new
+// 4. create a route for comments/:id
+// 5. create a route for comments/:id/edit
+// 6. create a route for comments/:id/delete
+// 7. create a route for comments/:id/upvote
+// 8. create a route for comments/:id/downvote
