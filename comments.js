a1634e57f634af63c55ec12f0ade482b344110e5
Marcelo José Guerra Pereira <mjguerrap@gmail.com>
2024-07-13 18:27:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,13 @@
 // create web server
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
+// create a route
 app.get('/comments', function(req, res) {
-    res.render('pages/comments');
+  res.send('This is the comments page');
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/comments', function(req, res) {
-    var name = req.body.name;
-    var comment = req.body.comment;
-    var data = name + ": " + comment + "\n";
-    fs.appendFile('comments.txt', data, function(err) {
-        if (err) throw err;
-    });
-    res.render('pages/comments');
+// start server
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000');
 });
-
-app.listen(80);
-console.log('80 is the magic port');
