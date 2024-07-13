6889670deac9bd170ccc74246ca13315a5cfc387
Marcelo José Guerra Pereira <mjguerrap@gmail.com>
2024-07-13 18:36:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,5 @@
 // create web server
-// 1. create web server
-// 2. create a route for comments
-// 3. create a route for comments/new
-// 4. create a route for comments/:id
-// 5. create a route for comments/:id/edit
-// 6. create a route for comments/:id/delete
-// 7. create a route for comments/:id/upvote
-// 8. create a route for comments/:id/downvote
+const express = require('express');
+const app = express();
+app.use(express.json());
+const comments = require('./comments.json');
