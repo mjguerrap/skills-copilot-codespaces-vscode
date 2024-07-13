cac953b6a473cc33aa6845d68f31959288628bbc
Marcelo José Guerra Pereira <mjguerrap@gmail.com>
2024-07-13 18:39:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -3,3 +3,19 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const comments = require('./comments.json');
+
+// GET /comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+// POST /comments
+app.post('/comments', (req, res) => {
+    const { comment } = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
